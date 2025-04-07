457afbe391147e6c860465c89a6ba646f65417c1
sharafudheen.M <sharafumt.hidragen@gmail.com>
2025-04-07 16:05:35 +0530

    docker file issues fixed

--- back-end/server.ts
+++ back-end/server.ts
@@ -111,10 +111,11 @@ io.on("connection", (socket) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
